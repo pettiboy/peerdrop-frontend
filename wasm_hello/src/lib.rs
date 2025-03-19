49d017adc58a39002f900ccc15abe8bc442b6485
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2025-03-20 02:32:33 +0530

    feat: add verify signature function and remove old mono fn

--- wasm_hello/src/lib.rs
+++ wasm_hello/src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 
-use ed25519_dalek::{Signature, Signer, SigningKey};
+use ed25519_dalek::{Signature, Signer, SigningKey, VerifyingKey};
 use rand::rngs::OsRng;
 
 #[wasm_bindgen]
@@ -50,39 +50,34 @@ pub fn eddsa_sign_message(signing_message: &str, secret_key: &str) -> String {
     return signature_hex;
 }
 
-pub fn create_eddsa_keypair() {
-    let mut csprng = OsRng;
-    let signing_key: SigningKey = SigningKey::generate(&mut csprng);
+pub fn eddsa_verify_signature(message: &str, signature: &str, public_key: &str) -> bool {
+    // -- message
+    let message: &[u8] = message.as_bytes();
 
-    let ed_private_hex = hex::encode(signing_key.to_bytes());
-    let ed_public_hex = hex::encode(signing_key.verifying_key().to_bytes());
-    let string_message = "Sign this";
-
-    println!("Ed25519 Private Key (hex): {}", ed_private_hex);
-    println!("Ed25519 Public Key  (hex): {}", ed_public_hex);
-
-    let message: &[u8] = string_message.as_bytes();
-    let signature: Signature = signing_key.sign(message);
-
-    // convert signature to hex
-    let signature_hex = hex::encode(signature.to_bytes());
-
-    println!("signature: {}", signature);
-    println!("signatureHex: {}", signature_hex);
-
-    // reconstruct signature from hex string
-    let reconstructed_message = string_message.as_bytes();
-    let signature_bytes = hex::decode(signature_hex).unwrap();
+    // -- signature
+    let signature_bytes = hex::decode(signature).unwrap();
     let signature_array: &[u8; 64] = signature_bytes
         .as_slice()
         .try_into()
         .expect("Slice with incorrect length");
     let reconstructed_sign = Signature::from_bytes(signature_array);
 
-    let is_valid_sign = signing_key
-        .verify(reconstructed_message, &reconstructed_sign)
+    // -- public key
+    // decode pk from hex
+    let public_key_bytes = hex::decode(public_key).expect("invalid hex string for secret key");
+
+    // ensure its 32 bytes
+    let verifying_key_array: &[u8; 32] = &public_key_bytes
+        .try_into()
+        .expect("public key must be 32 bytes");
+
+    let verifying_key = VerifyingKey::from_bytes(verifying_key_array).expect("invalid public key");
+
+    let is_valid_sign = verifying_key
+        .verify_strict(message, &reconstructed_sign)
         .is_ok();
-    println!("is signature valid {}", is_valid_sign);
+
+    return is_valid_sign;
 }
 
 #[cfg(test)]
@@ -106,10 +101,9 @@ mod tests {
         let msg = "hello world";
         let signature = eddsa_sign_message(msg, &ed_keys.secret_key);
         println!("signature: {}", signature);
-    }
 
-    #[test]
-    fn test_create_eddsa_keypair() {
-        create_eddsa_keypair()
+        // verify this signature
+        let is_sign_valid = eddsa_verify_signature(msg, signature.as_str(), &ed_keys.public_key);
+        println!("is valid: {}", is_sign_valid);
     }
 }
